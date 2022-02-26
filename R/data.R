8e8b8d84474e3f1b6491d4c41ac4508b4c0366fe
noriakis <noriaki.sato53@gmail.com>
2022-02-26 17:31:27 +0900

    add usage to data.R

--- R/data.R
+++ R/data.R
@@ -1,9 +1,11 @@
 #' Example enrichment analysis result
 #'
 #' An example enrichment analysis result to be used for testing purpose
+#' @usage data(exampleEaRes)
 "exampleEaRes"
 
 #' Example gene expression data
 #'
 #' An example gene expression data to be used for testing purpose made by runif()
+#' @usage data(exampleGeneExp)
 "exampleGeneExp"
