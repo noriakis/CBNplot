5f2a9dab34310b7e9fe5d0cc598cb69a4f48d933
noriakis <nori@hgc.jp>
2023-04-07 16:32:34 +0900

    fixed warnings

--- R/data.R
+++ R/data.R
@@ -4,6 +4,7 @@
 #' The result was produced by running ReactomePA::enrichPathway() and
 #' subsequent clusterProfiler::setReadable() on `exampleGeneExp`.
 #' @usage data(exampleEaRes)
+#' @return example enrichment analysis result
 "exampleEaRes"
 
 #' Example gene expression data
@@ -12,4 +13,5 @@
 #' made by runif() for ERCC genes and 100 samples.
 #' No biological meanings can be obtained from the data.
 #' @usage data(exampleGeneExp)
+#' @return example gene expression
 "exampleGeneExp"
