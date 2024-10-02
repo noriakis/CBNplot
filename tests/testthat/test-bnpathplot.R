06b6c009c72415f1668a749310c96c3be67543e3
noriakis <nori@hgc.jp>
2024-10-02 11:28:36 +0900

    oaqc removal and renew test

--- tests/testthat/test-bnpathplot.R
+++ tests/testthat/test-bnpathplot.R
@@ -1,11 +1,11 @@
 test_that("bnpathplot: not produce errors in variou parameters", {
     data("exampleEaRes");data("exampleGeneExp");
     R <- 5
-    expect_error( bnpathplot(exampleEaRes, exampleGeneExp, R = R), NA)
-    expect_error( bnpathplot(exampleEaRes, exampleGeneExp, R = R, edgeLink=TRUE), NA)
-    expect_error( bnpathplot(exampleEaRes, exampleGeneExp, disc = T, R = R), NA)
+    expect_error( bnpathplot(exampleEaRes, exampleGeneExp, R = R, strThresh=0), NA)
+    expect_error( bnpathplot(exampleEaRes, exampleGeneExp, R = R, edgeLink=TRUE, strThresh=0), NA)
+    expect_error( bnpathplot(exampleEaRes, exampleGeneExp, disc = T, R = R, strThresh=0), NA)
 
     # Custom
     expect_error( bnpathplotCustom(exampleEaRes, exampleGeneExp,
-    R = R, glowEdgeNum=3, hub=3), NA)
+    R = R, glowEdgeNum=3, hub=3, strThresh=0), NA)
 })
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -4,7 +4,7 @@ test_that("utilities: not produce errors in variou parameters", {
     data("exampleEaRes")
     data("exampleGeneExp")
     net <- bngeneplot(exampleEaRes, exampleGeneExp, R=5,
-                   pathNum=1, returnNet=TRUE)
+                   pathNum=1, returnNet=TRUE, strThresh=0)
     fitted <- bn.fit(net$av, net$df)
     res <- queryCpDistLw(fitted, candidate="ERCC4", evidence="ERCC2",
                           levels=c(0.1, 0.5, 0.8), n=50, alpha=TRUE)
